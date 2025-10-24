ee23cc49a751135bc934e13b637385a5d2d4ff6f
AAZ <amelias.azzi@gmail.com>
2025-10-25 01:25:17 +0200

    ✨ Can export recipe

--- src/modules/recipe/components/RecipeItem.tsx
+++ src/modules/recipe/components/RecipeItem.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
+import FileDownloadIcon from "@mui/icons-material/FileDownload";
 import { StepList } from "./StepList";
 import { StepModal } from "./StepModal";
 import type { Recipe } from "../../../interfaces";
 import { useRecipesContext } from "../context/RecipesContext";
 import "./RecipeItem.css";
+import { validateRecipe } from "../utils/validation";
 
 export const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
   const [isOpen, setIsOpen] = useState(false);
@@ -15,17 +17,38 @@ export const RecipeItem = ({ recipe }: { recipe: Recipe }) => {
     setIsOpen((prev) => !prev);
   };
 
+  const handleExport = (recipe: Recipe) => {
+    if (!validateRecipe(recipe)) {
+      alert("Recipe is invalid. Check all steps.");
+      return;
+    }
+
+    const dataStr = JSON.stringify(recipe, null, 2);
+    const blob = new Blob([dataStr], { type: "application/json" });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `${recipe.name}.json`;
+    link.click();
+    URL.revokeObjectURL(url);
+  };
+
   return (
     <div className="recipe-item">
       <div className="recipe-item-header">
         <div className="recipe-name-container" onClick={toggle}>
           <h3>{recipe.name}</h3>
         </div>
-        <div
-          className="icon-container"
-          onClick={() => setOpenAddStepModal(true)}
-        >
-          <AddOutlinedIcon sx={{ color: "#00cfb4" }} />
+        <div className="recipe-icons">
+          <div className="icon-container" onClick={() => handleExport(recipe)}>
+            <FileDownloadIcon sx={{ color: "#00cfb4" }} />
+          </div>
+          <div
+            className="icon-container"
+            onClick={() => setOpenAddStepModal(true)}
+          >
+            <AddOutlinedIcon sx={{ color: "#00cfb4" }} />
+          </div>
         </div>
       </div>
       {isOpen && (
