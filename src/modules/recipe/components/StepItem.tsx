77c5da44b923435c9cd3e1e5c13110f3b3f1ac29
AAZ <amelias.azzi@gmail.com>
2025-10-24 23:05:23 +0200

    💄 Fix stepItem UI

--- src/modules/recipe/components/StepItem.tsx
+++ src/modules/recipe/components/StepItem.tsx
@@ -1,7 +1,8 @@
 import { StepType } from "../../../constants/enums";
 import type { Step, TakeImageStep, UnscrewingStep } from "../../../interfaces";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
-import { useRecipesContext } from "../../../recipe/context/RecipesContext";
+import { useRecipesContext } from "../../recipe/context/RecipesContext";
+import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import "./StepItem.css";
 
 export const StepItem = ({
@@ -15,22 +16,34 @@ export const StepItem = ({
 
   return (
     <div className="step-item">
-      <div className="step-info">
-        <p>
-          {step.type === StepType.TakeImage
-            ? (step as TakeImageStep).takeImageType
-            : (step as UnscrewingStep).unscrewingType}
-        </p>
-        {step.coords && (
-          <p> {`coords: (x: ${step.coords.x}, y: ${step.coords.y})`}</p>
-        )}
-        <p> point cloud </p>
+      <div className="step-header">
+        <DragIndicatorIcon fontSize="small" />
+        <div className="step-columns">
+          <div className="step-col type">
+            {step.type === StepType.TakeImage
+              ? (step as TakeImageStep).takeImageType
+              : (step as UnscrewingStep).unscrewingType}
+          </div>
+
+          <div className="step-col coords">
+            {step.coords
+              ? `coords: (x: ${step.coords.x}, y: ${step.coords.y})`
+              : "—"}
+          </div>
+          <div className="step-col pointcloud">
+            {step.type === StepType.TakeImage &&
+            (step as TakeImageStep).pointCloud
+              ? "Yes"
+              : "No"}
+          </div>
+        </div>
       </div>
+
       <div
         className="icon-container"
         onClick={() => removeStep(recipeId, step.id)}
       >
-        <DeleteForeverIcon sx={{ color: "red" }} />
+        <DeleteForeverIcon sx={{ color: "red" }} fontSize="small" />
       </div>
     </div>
   );
